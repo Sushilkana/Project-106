978529bc5a8252cdd972b49d186aa683decbf6dc
Sushilkana <86962755+Sushilkana@users.noreply.github.com>
2022-01-22 20:44:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/e97PR4jLO/model.json', model_loaded);
+function Startclassification() {
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/e97PR4jLO/model.json",model_ready);
+}
+
+function model_ready(){
+    classifier.classify(gotresult);
+}
+
+function gotresult(error,result){
+    console.log(result);
+}
