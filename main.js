3c568c2d9f69e0b206e3a3dcf23b70f6983d77bd
Sushilkana <86962755+Sushilkana@users.noreply.github.com>
2022-01-30 15:52:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,51 @@ function model_ready(){
     classifier.classify(gotresult);
 }
 
-function gotresult(error,result){
-    console.log(result);
+function gotresult(error,results){
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(result);
+        document.getElementById("result_label").innerHTML = "I can hear- " + result[0].label;
+        document.getElementById("result_confidence").innerHTML = "Accuary- " + (result[0].confidence * 100).toFixed(2) + "%";
+
+        img1 = document.getElementById('img_1');
+        img2 = document.getElementById('img_2');
+        img3 = document.getElementById('img_3');
+        img4 = document.getElementById('img_4');
+        img5 = document.getElementById('img_5');
+
+
+        if (results[0].label == "Background Noise") {
+            img1.src = 'Sound Wave.gif';
+            img2.src = 'Dog.jpg';
+            img3.src = 'cat.jpg';
+            img4.src = 'Cow.png';
+            img5.src = 'Lion.jpg';
+        } else if (results[0].label == "Barking"){
+            img1.src = 'Sound wave.jpg';
+            img2.src = 'Dog.gif';
+            img3.src = 'cat.jpg';
+            img4.src = 'Cow.png';
+            img5.src = 'Lion.jpg';
+        } else if (results[0].label == "Meowing"){ 
+            img1.src = 'Sound Wave.jpg';
+            img2.src = 'Dog.jpg';
+            img3.src = 'Cat.gif';
+            img4.src = 'Cow.png';
+            img5.src = 'Lion.jpg';
+        } else if (results[0].label == "Mooing"){ 
+            img1.src = 'Sound wave.jpg';
+            img2.src = 'Dog.jpg';
+            img3.src = 'cat.jpg';
+            img4.src = 'Cow.gif';
+            img5.src = 'Lion.jpg';
+        } else { 
+            img1.src = 'Sound wave.jpg';
+            img2.src = 'Dog.jpg';
+            img3.src = 'cat.jpg';
+            img4.src = 'Cow.png';
+            img5.src = 'Lion.gif';
+        }
+    }
 }
